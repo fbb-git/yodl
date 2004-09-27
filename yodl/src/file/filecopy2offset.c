75612bd6ca6911fce0f75d91a13d439d2ceea728
Frank B. Brokken <f.b.brokken@rug.nl>
2004-09-27 19:21:09 +0000

    Repaired bug: file_copy2offset() was called with dest- and src-fileargs swapped.
    git-svn-id: https://stealth.svn.sourceforge.net/svnroot/yodl/trunk@30 7de777c3-3b1c-0410-83c5-80958bd9f4b1

--- yodl/src/file/filecopy2offset.c
+++ yodl/src/file/filecopy2offset.c
@@ -4,7 +4,7 @@ void file_copy2offset(FILE *dest, FILE *src, long src_end)
 {
     register char *buffer = new_memory(BLOCK_FILE, sizeof(char));
     long tocopy = src_end - ftell(src);
-
+ 
     if (tocopy <= 0)                            /* program may have eaten   */
     {                                           /* some bytes, yet          */
         int nread;
@@ -22,21 +22,21 @@ void file_copy2offset(FILE *dest, FILE *src, long src_end)
 
     while
     (
-        tocopy > BLOCK_FILE
-        &&
-        fread(buffer, BLOCK_FILE, 1, src) == 1
-        &&
-        fwrite(buffer, BLOCK_FILE, 1, dest) == 1
+        tocopy > BLOCK_FILE                     /* more than 1 block waits  */
+        &&                                      /* AND */
+        fread(buffer, BLOCK_FILE, 1, src) == 1  /* we read 1 block */
+        &&                                      /* AND */
+        fwrite(buffer, BLOCK_FILE, 1, dest) == 1/* we wrote 1 block */
     )
         tocopy -= BLOCK_FILE;
 
     if                                          /* copy the last block      */
     (
-        tocopy <= BLOCK_FILE
-        &&
-        fread(buffer, tocopy, 1, src) == 1
-        &&
-        fwrite(buffer, tocopy, 1, dest) == 1
+        tocopy <= BLOCK_FILE                    /* at most 1 block waits    */
+        &&                                      /* AND */
+        fread(buffer, tocopy, 1, src) == 1      /* we read the req'd chars  */
+        &&                                      /* AND */
+        fwrite(buffer, tocopy, 1, dest) == 1    /* we wrote the req'd chars */
     )
     {
         free(buffer);
