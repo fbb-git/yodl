09a62a7194098d41161f932fe81b2d8ab67e66be
Frank B. Brokken <f.b.brokken@rug.nl>
2017-05-26 13:02:51 +0200

    Added comments to chartab/chartabapply.c and parser/phandleinsert.c about     handling character translations. Chartab translations will be moved from     the parser to ostream.

--- yodl/src/chartab/chartabapply.c
+++ yodl/src/chartab/chartabapply.c
@@ -4,6 +4,8 @@ String *chartab_apply(char const *txt)
 {
     register String *sp = string_new(0);
 
+    // d_active is an array od 256 characters whose values are the 
+    // characters themselves 
     while (*txt)
         string_addstr(sp, 
                 chartab.d_active[*(unsigned char const *)txt++]);
--- yodl/src/parser/phandleinsert.c
+++ yodl/src/parser/phandleinsert.c
@@ -6,3 +6,16 @@ bool p_handle_insert(register Parser *pp)
 
     return true;
 }
+
+/*  Possibilities:
+        p_insert_chartab_string;        -> chartab_apply
+        p_insert_chartab_ostream;       -> chartab_apply
+        p_insert_no_chartab_string;
+        p_insert_no_chartab_ostream;
+        p_insert_nothing
+
+    chartab_apply visits all characters of a string and converts them to the
+    converted value. The converted string is returns. The characters to
+    convert are obtained from the text matching the last TOKEN returned by the 
+    lexer 
+*/
