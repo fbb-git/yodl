c0606f8413a2d23d13f2ede6b83e711a8bea87f2
Frank B. Brokken <f.b.brokken@rug.nl>
2017-12-15 12:57:09 +0100

    implemented \\ as eoln comment marker

--- yodl/lexer/lnextchar.c
+++ yodl/lexer/lnextchar.c
@@ -1,36 +1,25 @@
 #include "lexer.ih"
 
-/*
-    Nextchar determines the next character to return.
-    It is:
-
-        - reading a \-character:
-            - \ if it is not followed by a \n
-            - the next non-(space or tab) character if \ was followed by a \n
-                (so use backslash + 2 newlines to return one newline)
-        - reading a \n -char:
-            if blanks at the end of the line can be skipped:
-
-
-
-
-*/
-
 void l_nextchar(register Lexer *lp)
 {
-    register int ch = l_get(lp);            /* read from file or buffer     */
+    register int ch = l_get(lp);            // read from file or buffer
 
-    while (ch == '\\')                      /* a backslash: handled         */
-    {                                       /* traditionally                */
-        ch = l_get(lp);                     /* read the next one            */
+    while (ch == '\\')                      // a backslash
+    {
+        ch = l_get(lp);                     // get the next char
+        if (ch == '\\')                     // another backslash: skip all
+        {                                   // chars until newline
+            while ((ch = l_get(lp)) != '\n')
+                ;
+        }
 
-        if (ch != '\n')                     /* not a \EOLN                  */
+        if (ch != '\n')                     // not EOLN
         {
             l_unget(lp, ch);                /* unget that char              */
             lp->d_lastchar = '\\';          /* and return \                 */
             return;
         }
-                                            /* skip ' ', '\t' after \EOLN   */
+                                            // skip ' ', '\t' after EOLN
         while ((ch = l_get(lp)) == ' ' || ch == '\t')
             ;
     }
@@ -49,3 +38,25 @@ void l_nextchar(register Lexer *lp)
     lp->d_lastchar = ch;
     return;                                 /* done: return the char.       */
 }
+
+/*
+    Nextchar determines the next character to return.
+    The next char is:
+
+        - when reading a \-character:
+            - the next non-(space or tab) character if \ is the last character
+                on a line (so backslash + 2 newlines to return one newline)
+            - if immediately followed by another \ all subsequent chars on
+                that line are skipped, as are all space- and tab characters on
+                the next line (this allows end-of-line comment to be added to
+                e.g., macro definitions). To 
+            - \ if not the last character on the line or not followed by a
+                second \ character. To write a single \ at the end of a line
+                use, e.g., CHAR(\) at the end of a line. To skip all ws
+                characters thereafter use, e.g., CHAR(\)\.
+        - when reaching the end of the line, 
+            if blanks at the end of the line can be skipped: the first
+            non-blank character on the next line.
+        - otherwise the next character is returned.
+*/
+
